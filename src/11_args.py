5ac2268c1d2b02094a39c1d94750a6733591d47f
Dom Eccleston <domeccleston@gmail.com>
2020-02-11 12:30:01 +0000

    fix errors

--- src/11_args.py
+++ src/11_args.py
@@ -32,7 +32,7 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -43,7 +43,7 @@ print(f2(a))    # Should print 22
 
 
 def f3(*args):
-    if (args[1]):
+    if len(args) == 2:
         return sum(args)
     else:
         return args[0] + 1
@@ -85,6 +85,6 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+# f4(d)
 
 f4(**{'monster': 'goblin', 'hp': 3})
